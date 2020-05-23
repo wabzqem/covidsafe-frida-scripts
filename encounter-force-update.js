aae134a2a03a8e1818a6f93bde570514c48c1ba7
Richard Nelson <richard@richardnelson.it>
2020-05-23 22:30:48 +1000

    Update encounter-force-update.js to update both ADVT_DAT and BROADCAST_MSG

--- encounter-force-update.js
+++ encounter-force-update.js
@@ -15,19 +15,21 @@ function setExpiryNow() {
   var myDate = ObjC.classes.NSDate.alloc().init();
   var userdefaults = ObjC.classes.NSUserDefaults.alloc().init();
   userdefaults.setObject_forKey_(myDate, ObjC.classes.NSString.stringWithString_('ADVT_EXPIRY'));
+  userdefaults.removeObjectForKey_(ObjC.classes.NSString.stringWithString_('ADVT_DATA'));
   console.log("Set ADVT_EXPIRY to " + myDate.toString() + "\n");
 }
 
 function printUserDefaults() {
   var userdefaults = ObjC.classes.NSUserDefaults;
   var dict = userdefaults.alloc().init().dictionaryRepresentation();
+  var data = dict.objectForKey_(ObjC.classes.NSString.stringWithString_('ADVT_DATA'))
   console.log(Date());
   console.log("BROADCAST_MSG: " + dict.objectForKey_(ObjC.classes.NSString.stringWithString_('BROADCAST_MSG')).toString());
-  console.log("ADVT_DATA: " + dict.objectForKey_(ObjC.classes.NSString.stringWithString_('ADVT_DATA')).toString());
+  console.log("ADVT_DATA: " + (data == null ? "null" : data.bytes().readUtf8String(data.length())));
   console.log("ADVT_EXPIRY: " + dict.objectForKey_(ObjC.classes.NSString.stringWithString_('ADVT_EXPIRY')).toString());
   console.log("\n");
 }
 
 setTimeout(printUserDefaults, 500);
-setInterval(printUserDefaults, 1000 * 30);
-setInterval(setExpiryNow, 1000 * 30);
+setInterval(printUserDefaults, 1000 * 60 * 5); // Print out values every 5 minutes
+setTimeout(function() { setInterval(setExpiryNow, 1000 * 60 * 15); }, 1000 * 5) // Force expiry every 15 minutes
